1b903cfe6515666dbc2d0c4a3ab82da2cde74452
VisualGMQ <2142587070@qq.com>
2024-12-03 19:46:37 +0800

    Resolve #62: add profiler

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,5 +1,8 @@
-#include "pch.hpp"
 #include "context.hpp"
+#include "pch.hpp"
+#include "profile.hpp"
+#include <ctime>
+#include <filesystem>
 
 #ifdef __EMSCRIPTEN__
 void MainLoop() {
@@ -8,17 +11,40 @@ void MainLoop() {
 #endif
 
 int main(int argc, char** argv) {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    tm* tm = std::localtime(&now_time);
+
+#ifdef TL_ENABLE_PROFILE
+    EASY_PROFILER_ENABLE
+    profiler::startListen();
+    auto curPath = std::filesystem::current_path();
+    curPath /= "log";
+    if (!std::filesystem::exists(curPath)) {
+        std::filesystem::create_directory(curPath);
+    }
+#endif
+
     tl::Context::Init();
     tl::Context& ctx = tl::Context::GetInst();
 
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(MainLoop, 0, true);
 #else
+
     while (!ctx.ShouldExit()) {
         ctx.Update();
     }
 #endif
     tl::Context::Destroy();
+
+#ifdef TL_ENABLE_PROFILE
+    char filename[1024] = {0};
+    snprintf(filename, sizeof(filename), "log/%d-%d-%d_%d-%d-%d.prof", tm->tm_year,
+             tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+    profiler::dumpBlocksToFile(filename);
+    profiler::stopListen();
+#endif
 
     return 0;
 }
