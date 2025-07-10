106c2881f46d7acd23c9152e6d9ce6f8d6eba744
VisualGMQ <35966247+VisualGMQ@users.noreply.github.com>
2025-07-10 12:45:24 +0800

    Resolve #5: add CI and ReadMe

---
+++ android/app/src/main/java/org/libsdl/hello/HelloActivity.java
@@ -0,0 +1,8 @@
+
+package org.libsdl.hello;
+
+import org.libsdl.app.SDLActivity;
+
+public class HelloActivity extends SDLActivity
+{
+}
--- android/org.visualgmq.tl/app/src/main/java/org/visualgmq/tl/TlActivity.java
+++
@@ -1,8 +0,0 @@
-
-package org.visualgmq.tl;
-
-import org.libsdl.app.SDLActivity;
-
-public class TlActivity extends SDLActivity
-{
-}
