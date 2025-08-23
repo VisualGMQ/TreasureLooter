019893da2782fb7c66d241a5c5c407e629de42b2
VisualGMQ <2142587070@qq.com>
2025-08-24 02:50:18 +0800

    add stb_vorbis

---
+++ game/include/music.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "path.hpp"
+
+class Sound {
+public:
+    explicit Sound(const Path& filename);
+    ~Sound();
+
+private:
+    short* m_data{};
+};
---
+++ game/src/music.cpp
@@ -0,0 +1,18 @@
+﻿#include "music.hpp"
+#include "storage.hpp"
+#define STB_VORBIS_HEADER_ONLY
+#include "stb_vorbis.h"
+
+Sound::Sound(const Path& filename) {
+    auto file = IOStream::CreateFromFile(filename, IOMode::Read, true);
+    auto content = file->Read();
+
+    int channels, sample_rate;
+    short* data;
+
+    int samples = stb_vorbis_decode_memory((const unsigned char*)content.data(), content.size(), &channels, &sample_rate, &data);
+}
+
+Sound::~Sound() {
+    delete m_data;
+}
